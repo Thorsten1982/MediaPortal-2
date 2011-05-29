df0c196e96c11d668ddbdb839fb5807099fb568c
Albert <albert@team-mediaportal.com>
2011-05-29 09:25:14 +0200

    Additional variables and initialization code for profiling theme and screen control allocations

--- MediaPortal/Source/UI/SkinEngine/SkinManagement/XamlLoader.cs
+++ MediaPortal/Source/UI/SkinEngine/SkinManagement/XamlLoader.cs
@@ -39,6 +39,11 @@ namespace MediaPortal.UI.SkinEngine.SkinManagement
   /// </summary>              
   public class XamlLoader
   {
+    /// <summary>
+    /// Loader property key whose value is set to <c>true</c> if the theme is currently loading, else it is <c>false</c>.
+    /// </summary>
+    public const string KEY_IS_THEME = "Theme";
+
     public const string KEY_ACTIVATE_BINDINGS = "ActivateBindings";
 
     /// <summary>
@@ -48,14 +53,15 @@ namespace MediaPortal.UI.SkinEngine.SkinManagement
     /// <param name="loader">Loader callback for GUI models.</param>
     /// <param name="activateBindings">If set to <c>true</c>, bindings will be activated, else they will be left
     /// unactivated.</param>
+    /// <param name="isTheme">Set to <c>true</c> if the theme is loading. If the skin is loading, it is <c>false</c>.</param>
     /// <returns><see cref="UIElement"/> descendant corresponding to the root element in the
     /// specified skin file.</returns>
-    public static object Load(string skinFilePath, IModelLoader loader, bool activateBindings)
+    public static object Load(string skinFilePath, IModelLoader loader, bool activateBindings, bool isTheme)
     {
       try
       {
         using (TextReader reader = new StreamReader(skinFilePath))
-          return Load(reader, loader, activateBindings);
+          return Load(reader, loader, activateBindings, isTheme);
       }
       catch (XamlLoadException e)
       {
@@ -75,9 +81,10 @@ namespace MediaPortal.UI.SkinEngine.SkinManagement
     /// <param name="loader">Loader callback for GUI models.</param>
     /// <param name="activateBindings">If set to <c>true</c>, bindings will be activated, else they will be left
     /// unactivated.</param>
+    /// <param name="isTheme">Set to <c>true</c> if the theme is loading. If the skin is loading, it is <c>false</c>.</param>
     /// <returns><see cref="UIElement"/> descendant corresponding to the root element in the
     /// specified skin file.</returns>
-    public static object Load(TextReader reader, IModelLoader loader, bool activateBindings)
+    public static object Load(TextReader reader, IModelLoader loader, bool activateBindings, bool isTheme)
     {
       try
       {
@@ -85,6 +92,7 @@ namespace MediaPortal.UI.SkinEngine.SkinManagement
         parser.SetCustomTypeConverter(MPF.ConvertType);
         parser.SetContextVariable(typeof(IModelLoader), loader);
         parser.SetContextVariable(KEY_ACTIVATE_BINDINGS, activateBindings);
+        parser.SetContextVariable(KEY_IS_THEME, isTheme);
         return parser.Parse(activateBindings);
       }
       catch (Exception e)
